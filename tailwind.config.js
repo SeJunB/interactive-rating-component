c1a81aef16dd1a20fcbddb9b4d76d82eff526a21
SeJun Bae <sejun.bae@outlook.com>
2023-04-05 09:24:56 -0700

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.tsx'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./index.html', './src/**/*.tsx'],
+    theme: {
+        extend: {
+            colors: {
+                'orange': 'hsl(25, 97%, 53%)',
+                'light-gray': 'hsl(217, 12%, 63%)',
+                'medium-grey': 'hsl(216, 12%, 54%)',
+                'dark-blue': 'hsl(213, 19%, 18%)',
+                'very-dark-blue': 'hsl(216, 12%, 8%)'
+            }
+        },
+    },
+    plugins: [],
 }
 
