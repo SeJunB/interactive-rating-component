503b6f29acd25fc03fe4a78f075c5e32407bafd7
SeJun Bae <sejun.bae@outlook.com>
2023-04-06 16:42:10 -0700

    Adjust tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
     content: ['./index.html', './src/**/*.tsx'],
     theme: {
         extend: {
+            screens: {
+                'sm': '500px',
+                'md': '610px',
+                'lg': '850px',
+                
+            },
             colors: {
                 'orange': 'hsl(25, 97%, 53%)',
                 'primary-bg': 'hsl(215, 23%, 14%)',
