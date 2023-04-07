9df955afeb6d60565eb5b3a19b5235d32b16b226
SeJun Bae <sejun.bae@outlook.com>
2023-04-07 08:03:48 -0700

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/result-summary-component/',
+    base: '/interactive-rating-component/',
     plugins: [react()],
 })
